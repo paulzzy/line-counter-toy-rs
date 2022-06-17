f5a3f98b43bae35f09518fbb294bdc906c44c981
Paul Zhang <paulz@ucla.edu>
2022-06-16 18:58:10 -0700

    Complete Week 2: counting lines in file or stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let input = env::args().nth(1).expect("no input");
+
+    let reader: Box<dyn BufRead> = match input.as_ref() {
+        "-" => Box::new(BufReader::new(io::stdin())),
+        _ => Box::new(BufReader::new(File::open(&input).expect("file not found"))),
+    };
+
+    let count = reader.lines().count();
+
+    println!("{}", count);
 }
